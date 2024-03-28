078e8417829c51e3a32c7e0e98d5512456ec571d
unknown <abakharev74@gmail.com>
2024-03-28 18:53:54 +0500

    kittygram_final 0.7

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['kittygram.freedynamicdns.net']
+ALLOWED_HOSTS = ['127.0.0.1', '84.201.139.102', 'localhost', 'kittygram.freedynamicdns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
